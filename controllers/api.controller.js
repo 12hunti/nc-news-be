79a65b4cfc024af396719afd9d50488cfcf86eb5
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-03-10 11:43:27 +0000

    update api controller formatting

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,5 @@
-const endpoints = require("../endpoints.json")
+const endpoints = require("../endpoints.json");
 
 exports.getApi = (request, response) => {
-    response.send({endpoints})
-}
+  response.send({ endpoints });
+};
