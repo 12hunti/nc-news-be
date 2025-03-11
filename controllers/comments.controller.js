c32b2df503bd5ba98ac94b71371a6703e2bfc172
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-03-11 15:39:57 +0000

    ticket 8 passing 204 test

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,6 +1,7 @@
 const {
   fetchCommentsByArticleId,
   insertCommentsByArticleId,
+  removeCommentsbyCommentId,
 } = require("../models/comments.model");
 
 exports.getCommentsByArticleId = (request, response, next) => {
@@ -16,11 +17,19 @@ exports.getCommentsByArticleId = (request, response, next) => {
 
 exports.postCommentsByArticleId = (request, response, next) => {
   const { article_id } = request.params;
-  const {  author, body } = request.body;
-  insertCommentsByArticleId(author, body, article_id).then((newComment) => {
-    response.status(201).send({ newComment });
-  })
-  .catch((err) => {
-    next(err)
-  })
+  const { author, body } = request.body;
+  insertCommentsByArticleId(author, body, article_id)
+    .then((newComment) => {
+      response.status(201).send({ newComment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteCommentByCommentId = (request, response, next) => {
+    const {comment_id} = request.params
+    removeCommentsbyCommentId(comment_id).then((deletedComment) => {
+        response.status(204).send({deletedComment})
+    })
 };
