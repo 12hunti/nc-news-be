09480d4ba63688a062ade53b5565c1c4ba19d8f2
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-03-11 10:40:02 +0000

    ticket 5 update to pass additional test

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -78,15 +78,17 @@ const formatComments = (commentData, insertArticles) => {
 
 const checkExists = (table, column, value) => {
   const queryString = format(`SELECT * FROM %I WHERE %I = $1`, table, column);
-  return db.query(queryString, [value]).then(({rows}) => {
-    if (rows.length === 0){
-      return Promise.reject({ status: 404, msg: "resource not found" })
-    }
-    return true
-  })
-  .catch((err) => {
-    return Promise.reject(err)
-  })
+  return db
+    .query(queryString, [value])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "resource not found" });
+      }
+      return true;
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
 };
 
 module.exports = {
