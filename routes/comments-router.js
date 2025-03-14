c45534a83c606fe501e99d9b591208cd6a41508a
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-03-14 15:57:37 +0000

    ticket 17 passing 200 test

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,9 +1,11 @@
 const commentsRouter = require("express").Router();
 
 const {
-  deleteCommentByCommentId,
+  deleteCommentByCommentId, patchCommentByCommentID
 } = require("../controllers/comments.controller");
 
 commentsRouter.delete("/:comment_id", deleteCommentByCommentId);
+
+commentsRouter.patch("/:comment_id", patchCommentByCommentID)
 
 module.exports = commentsRouter;
